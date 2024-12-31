7ca7cb161d9f499d7e18fcb9db2add31f0f54ebc
sgabenza <sgabenza86@gmail.com>
2024-12-31 12:04:12 +0100

    [prismic] homepage data

--- app/[uid]/page.tsx
+++ app/[uid]/page.tsx
@@ -2,9 +2,10 @@ import { createClient } from '@/prismicio'
 
 export default async function Page() {
   const client = createClient()
+  console.log('client: ', client);
 
-  const page = await client.getByUID('archive', 'potpotpot')
-  console.log('page: ', page.data.potpotpot);
+  // const page = await client.getByUID('archive', 'potpotpot')
+  // console.log('page: ', page.data.potpotpot);
 
-  return <h1>{page.uid}</h1>
+  return <h1>{'page.uid'}</h1>
 }
