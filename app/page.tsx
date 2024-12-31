02e1bde4bd36fba54d23abd88bbfb62543abca8a
sgabenza <sgabenza86@gmail.com>
2024-12-31 14:05:05 +0100

    [homepage] hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
 import { createClient } from '@/prismicio'
 import { asLink } from '@prismicio/client'
+import Image from 'next/image'
 
 export default async function Home() {
   const client = createClient()
-  console.log('client: ', client)
 
   const page = await client.getSingle('homepage')
   const { facebook, hero, instagram, products, website } = page.data
 
   return (
     <div>
-      <div className="hero h-[654px] bg-gray-300 mb-4"></div>
+      <div className="bg-gray-300 mb-4 w-full h-[654px]">
+        <Image
+          className="object-cover"
+          src={hero.url ?? ''}
+          alt="HERO PICTURE"
+          width={1440}
+          height={654}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            maxHeight: 654,
+          }}
+        />
+      </div>
 
       <div className="p-4">
         <div className="text-center mb-24">
@@ -18,12 +31,19 @@ export default async function Home() {
           <div className="text-stone">Potteries by Leonardo Romano</div>
         </div>
         <div className="bg-red-300 h-96"></div>
+
         <div className="text-center my-4">Discover all</div>
         <footer className="flex justify-between my-4">
-          <a href={asLink(website) as string} target='_blank'>PotPotPottery</a>
+          <a href={asLink(website) as string} target="_blank">
+            PotPotPottery
+          </a>
           <div className="text-stone flex gap-5">
-            <a href={asLink(instagram) as string} target='_blank'>Instagram</a>
-            <a href={asLink(facebook) as string} target='_blank'>Facebook</a>
+            <a href={asLink(instagram) as string} target="_blank">
+              Instagram
+            </a>
+            <a href={asLink(facebook) as string} target="_blank">
+              Facebook
+            </a>
           </div>
         </footer>
       </div>
