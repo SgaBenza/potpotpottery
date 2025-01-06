a0676703725d46d8c8fa3184412508eef5cec8f7
sgabenza <sgabenza86@gmail.com>
2025-01-06 18:39:34 +0100

    [single] fix props

--- app/archive/@single/(..)products/[uid]/page.tsx
+++ app/archive/@single/(..)products/[uid]/page.tsx
@@ -1,6 +1,6 @@
 import { SingleModal } from '@/components/SinglePotModal'
 
-export default async function Single({ params }: { params: { uid: string } }) {
-  const uid = (await params).uid
+export default async function Single({ params }: { params: Promise<{ uid: string }> }) {
+  const uid = (await params)?.uid
   return <SingleModal uid={uid} />
 }
