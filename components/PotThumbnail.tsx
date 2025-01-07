40df0158614d7e37be839665c88b009d64744f69
sgabenza <sgabenza86@gmail.com>
2025-01-07 17:22:37 +0100

    [responsive] homepage

--- components/PotThumbnail.tsx
+++ components/PotThumbnail.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image'
 
 export const PotThumbnail = ({ image, name, price }: Pot) => {
   return (
-    <div className="relative h-[634px] w-[453px] hover-items">
-      <Image
+    <div className="relative h-[475px] w-full xl:h-[634px] xl:w-[453px] hover-items" style={{border:'solid'}}>
+      {/* <Image
         src={image.url ?? ''}
         alt="pot image"
         width={453}
         height={453}
         style={{ maxHeight: 453, objectFit: 'cover' }}
-      />
-      <div className="absolute bottom-0 left-0 right-0">
+      /> */}
+      <div className="xl:absolute bottom-0 left-0 right-0">
         <div className="h-14">{name}</div>
         <div className="h-14">{formatPrice(price)}</div>
       </div>
